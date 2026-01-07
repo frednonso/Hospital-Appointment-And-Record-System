f2be8e324a05576b31c905ced18d1469e9b16efd
Frednonso <hdmelchizedek@gmail.com>
2026-01-07 10:18:55 +0100

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,25 @@ Route::get('/user', function (Request $request) {
 
 
 
-Route::middleware("auth:sanctum")->group(function() {
-    
-    
+// Auth Routes
+Route::post('/register', [App\Http\Controllers\RegisterUserController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth Routes (Protected)
+    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+
+    // Appointment Routes
+    Route::post('/appointments/book', [App\Http\Controllers\AppointmentController::class, 'book']);
+    Route::post('/appointments/{appointment}/approve', [App\Http\Controllers\AppointmentController::class, 'approve']);
+    Route::post('/appointments/{appointment}/reject', [App\Http\Controllers\AppointmentController::class, 'reject']);
+
+    // Medical Record Routes
+    Route::post('/medical-records', [App\Http\Controllers\MedicalRecordController::class, 'create']);
+    Route::put('/medical-records/{id}', [App\Http\Controllers\MedicalRecordController::class, 'update']);
+    Route::get('/medical-records/doctor', [App\Http\Controllers\MedicalRecordController::class, 'Doctorsview']);
+    Route::get('/medical-records/patient', [App\Http\Controllers\MedicalRecordController::class, 'Patientsview']);
+
+    // Dashboard Routes
+    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index']);
 });
